288725d397405809cd49af3990b1ecdd8470d632
joker081s <nandalsahil023@gmail.com>
2023-01-17 19:20:15 +0530

    First HelloJava Program 2

--- HelloJava.java
+++ HelloJava.java
@@ -5,11 +5,25 @@ public class HelloJava {
     }
 
     public static void pattern() {
+
         for (int i = 0; i <= 4; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+
+        System.out.println("================================");
+
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 3-i; j >= 0; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
 }
